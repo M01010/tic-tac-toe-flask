7f529b1ac3d4c8c3d3df5712a598e148321a1033
M01010 <ihaamoodi@gmail.com>
2023-10-22 11:32:38 +0300

    initial commit

---
+++ game.py
@@ -0,0 +1,141 @@
+from player import Player
+
+
+def row_col_to_num(i, j):
+    return (i * 3) + j + 1
+
+
+def num_to_row_col(num):
+    num -= 1
+    j = num % 3
+    i = (num - j) // 3
+    return i, j
+
+
+class Game:
+
+    def __init__(self):
+        self.matrix = [
+            [' ', ' ', ' '],
+            [' ', ' ', ' '],
+            [' ', ' ', ' '],
+        ]
+        self.set_user(Player.Max)
+
+    def set_user(self, user):
+        self.user = user
+        self.matrix = [
+            [' ', ' ', ' '],
+            [' ', ' ', ' '],
+            [' ', ' ', ' '],
+        ]
+        if self.user == Player.Max:
+            self.bot = Player.Min
+        else:
+            self.bot = Player.Max
+
+    def evaluate(self) -> int:
+        def check(x: str):
+            for i in range(3):
+                if self.matrix[i][0] == self.matrix[i][1] == self.matrix[i][2] == x:
+                    return True
+            for i in range(3):
+                if self.matrix[0][i] == self.matrix[1][i] == self.matrix[2][i] == x:
+                    return True
+            if self.matrix[0][0] == self.matrix[1][1] == self.matrix[2][2] == x:
+                return True
+            if self.matrix[0][2] == self.matrix[1][1] == self.matrix[2][0] == x:
+                return True
+            return False
+
+        if check(Player.Max.value):
+            return 10
+        if check(Player.Min.value):
+            return -10
+        return 0
+
+    def terminal(self) -> bool:
+        def check(x: str, y: str, z: str) -> bool:
+            return x == y == z and x != ' '
+
+        for i in range(3):
+            if check(self.matrix[i][0], self.matrix[i][1], self.matrix[i][2]):
+                return True
+        for i in range(3):
+            if check(self.matrix[0][i], self.matrix[1][i], self.matrix[2][i]):
+                return True
+        if check(self.matrix[0][0], self.matrix[1][1], self.matrix[2][2]):
+            return True
+        if check(self.matrix[0][2], self.matrix[1][1], self.matrix[2][0]):
+            return True
+        for i in range(3):
+            for j in range(3):
+                if self.matrix[i][j] == ' ':
+                    return False
+        return True
+
+    def maximize(self, depth=0) -> int:
+        if self.terminal():
+            return self.evaluate() + depth
+        val = -999
+        for i in range(3):
+            for j in range(3):
+                if self.matrix[i][j] == ' ':
+                    self.matrix[i][j] = Player.Max.value
+                    val = max(val, self.minimize(depth + 1))
+                    self.matrix[i][j] = ' '
+                    if val == 10 - depth:
+                        return val
+        return val
+
+    def minimize(self, depth=0) -> int:
+        if self.terminal():
+            return self.evaluate() - depth
+        val = 999
+        for i in range(3):
+            for j in range(3):
+                if self.matrix[i][j] == ' ':
+                    self.matrix[i][j] = Player.Min.value
+                    val = min(val, self.maximize(depth + 1))
+                    self.matrix[i][j] = ' '
+                    if val == 10 + depth:
+                        return val
+        return val
+
+    def minimax(self):
+        best_action = None
+        if self.bot == Player.Max:
+            highest = -999
+            for i in range(3):
+                for j in range(3):
+                    if self.matrix[i][j] == ' ':
+                        self.matrix[i][j] = self.bot.value
+                        val = self.minimize()
+                        self.matrix[i][j] = ' '
+                        if val > highest:
+                            highest = val
+                            best_action = i, j
+            self.matrix[best_action[0]][best_action[1]] = self.bot.value
+            return
+        else:
+            lowest = 999
+            for i in range(3):
+                for j in range(3):
+                    if self.matrix[i][j] == ' ':
+                        self.matrix[i][j] = self.bot.value
+                        val = self.maximize()
+                        self.matrix[i][j] = ' '
+                        if val < lowest:
+                            lowest = val
+                            best_action = i, j
+            self.matrix[best_action[0]][best_action[1]] = self.bot.value
+            return
+
+    def play_user(self, n):
+        if n > 9 or n < 1:
+            raise Exception('1 to 9')
+        row, col = num_to_row_col(n)
+        if self.matrix[row][col] == ' ':
+            self.matrix[row][col] = self.user.value
+        else:
+            raise Exception('chosen before')
---
+++ main.py
@@ -0,0 +1,60 @@
+import random
+
+import game
+from player import Player
+from game import Game
+from flask import Flask, render_template, redirect, url_for, flash
+
+app = Flask(__name__)
+app.secret_key = '123'
+g = Game()
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/<player_type>')
+def start_game(player_type):
+    if player_type == Player.Min.value:
+        g.set_user(Player.Min)
+        i, j = game.num_to_row_col(random.randint(1, 9))
+        g.matrix[i][j] = g.bot.value
+    elif player_type == Player.Max.value:
+        g.set_user(Player.Max)
+    return render_template('game.html', matrix=g.matrix)
+
+
+@app.route('/end')
+def end():
+    return render_template('game.html', matrix=g.matrix, end=True)
+
+
+@app.route('/play/<int:i>/<int:j>')
+def play(i, j):
+    if g.terminal():
+        return redirect(url_for('end'))
+    try:
+        g.play_user(game.row_col_to_num(i, j))
+        if g.terminal():
+            if g.evaluate() == 0:
+                flash('you tied', 'info')
+            else:
+                flash('you won', 'info')
+            return redirect(url_for('end'))
+        g.minimax()
+        if g.terminal():
+            if g.evaluate() == 0:
+                flash('you tied', 'info')
+            else:
+                flash('you lost', 'info')
+            return redirect(url_for('end'))
+        return render_template('game.html', matrix=g.matrix)
+    except Exception as e:
+        flash(f'{e}', 'danger')
+        return render_template('game.html', matrix=g.matrix)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
